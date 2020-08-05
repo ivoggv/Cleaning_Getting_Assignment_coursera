1474291547b0e11afd1a682d6240c1f5385fd34d
ivoggv <italivo3@gmail.com>
2020-08-05 09:58:45 -0600

    Usage of only write.table function and row.name=FALSE

--- run_analysis.R
+++ run_analysis.R
@@ -55,5 +55,4 @@ std=grep("*std*",x = names(dataset))
 dataset=dataset[,c(1:2,mean,std)]
 
 final_dataset=dataset %>% group_by(activity,subject) %>% summarise_all(funs(mean))
-write.csv(final_dataset,"final_dataset.txt")
-#write.table(final_dataset,"final_dataset.txt")
+write.table(final_dataset,"final_dataset.txt",row.names = FALSE)
